16fd5a3e7de440b098f6765f9291e0f9907c8dd3
Lucas Castro <lucascastrosantana5@gmail.com>
2022-02-08 14:31:05 -0300

    adicionado os arquivos

---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.css']
+})
+export class PokemonListComponent implements OnInit {
+  pokemons: any[] = [];
+  page = 1;
+  totalPokemons: number | undefined;
+
+  constructor(
+    private dataService: DataService
+  ) { }
+
+  ngOnInit(): void {
+    this.getpokemons();
+  }
+
+  //Get Pokemons
+  getpokemons(){
+      this.dataService.getPokemons(12, this.page - 1)
+      .subscribe((response: any) => {
+        this.totalPokemons = response.count;
+      
+      response.results.forEach((result: { name: string; })  =>  {
+        this.dataService.getMoreData(result.name)
+        .subscribe((uniqResponse: any) =>{
+          this.pokemons.push(uniqResponse);
+           console.log(this.pokemons);
+         });
+       });
+    });
+  }
+
+}
